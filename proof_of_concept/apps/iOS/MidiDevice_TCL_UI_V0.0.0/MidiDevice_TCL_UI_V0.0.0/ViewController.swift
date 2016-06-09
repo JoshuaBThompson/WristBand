691954d82241cafbbf3a4a4d21f3851cde68a18a
JoshuaBThompson <jbthompson.eng@gmail.com>
2016-06-09 09:03:11 -0700

    added code to test instrument preset selction with knob turns

--- proof_of_concept/apps/iOS/MidiDevice_TCL_UI_V0.0.0/MidiDevice_TCL_UI_V0.0.0/ViewController.swift
+++ proof_of_concept/apps/iOS/MidiDevice_TCL_UI_V0.0.0/MidiDevice_TCL_UI_V0.0.0/ViewController.swift
@@ -8,17 +8,52 @@
 
 import UIKit
 
+//Instrument structures
+enum InstrumentNumbers {case SNARE, KICK}
+enum InstrumentPresets {case PRESET1, PRESET2, PRESET3, PRESET4}
+struct Instrument {
+    var type: InstrumentNumbers = .SNARE
+    var preset: InstrumentPresets = .PRESET1
+}
+
+struct InstrumentCollection {
+    var instrument: Instrument!
+}
+
 class ViewController: UIViewController {
     
     //MARK: properties
-    
+    var snare = Instrument()
+    var kick = Instrument()
+    var instruments = InstrumentCollection()
 
-    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var knob: Knob!
+    
+    @IBOutlet weak var preset1Label: UILabel!
+    
+    @IBOutlet weak var preset2Label: UILabel!
+    
+    @IBOutlet weak var preset3Label: UILabel!
+    
+    @IBOutlet weak var preset4Label: UILabel!
+    //MARK: actions
+    
+    @IBAction func selectSnare(sender: UIButton) {
+        instruments.instrument = snare
+    }
+    
+    @IBAction func selectKick(sender: UIButton) {
+        instruments.instrument = kick
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        snare.type = .SNARE
+        kick.type = .KICK
+        instruments.instrument = snare
+        
         //https://developer.apple.com/library/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/Lesson3.html
-        ratingControl.addTarget(self, action: #selector(ViewController.knobAngleChanged(_:)), forControlEvents: .ValueChanged)
+        knob.addTarget(self, action: #selector(ViewController.knobAngleChanged(_:)), forControlEvents: .ValueChanged)
     }
 
     override func viewDidLayoutSubviews() {
@@ -30,10 +65,29 @@ class ViewController: UIViewController {
     }
     
     //MARK: Functions
-    func knobAngleChanged(ratingController: RatingControl) {
+    func knobAngleChanged(knobControl: Knob) {
         
-        let angle = ratingControl.knobAngle;
+        let angle = knobControl.knobAngle;
         print("knob angle now \(angle)")
+        if(angle <= 0 && angle >= -15){
+            instruments.instrument.preset = .PRESET1
+        }
+        else if(angle < -15 && angle >= -30){
+            instruments.instrument.preset = .PRESET2
+        }
+        else if(angle < -30 && angle >= -45){
+            instruments.instrument.preset = .PRESET3
+        }
+        else if(angle < -45 && angle >= -90){
+            instruments.instrument.preset = .PRESET4
+        }
+     
+        
+        //update color of preset labels
+        preset1Label.textColor = (instruments.instrument.preset == .PRESET1) ? UIColor.redColor() : UIColor.blackColor()
+        preset2Label.textColor = (instruments.instrument.preset == .PRESET2) ? UIColor.redColor() : UIColor.blackColor()
+        preset3Label.textColor = (instruments.instrument.preset == .PRESET3) ? UIColor.redColor() : UIColor.blackColor()
+        preset4Label.textColor = (instruments.instrument.preset == .PRESET4) ? UIColor.redColor() : UIColor.blackColor()
     }
 
 

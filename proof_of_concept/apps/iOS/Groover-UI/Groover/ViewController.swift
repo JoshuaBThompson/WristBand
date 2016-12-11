47a190dbaba63b7f163d3b526631f4c4f1230456
JoshuaBThompson <jbthompson.eng@gmail.com>
2016-12-10 16:10:59 -0800

    added ViewController.swift from Backend

--- proof_of_concept/apps/iOS/Groover-UI/Groover/ViewController.swift
+++ proof_of_concept/apps/iOS/Groover-UI/Groover/ViewController.swift
@@ -8,27 +8,8 @@
 
 import UIKit
 import CoreMotion
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        self.navigationController?.navigationBar.shadowImage =  UIImage()
-        self.navigationController?.navigationBar.isTranslucent = true
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-}
 
-/*import UIKit
+import UIKit
 import CoreMotion
 
 class ViewController: UIViewController {
@@ -38,83 +19,107 @@ class ViewController: UIViewController {
     let sensor = MidiSensorWrapper()
     let motionManager = CMMotionManager()
     let queue = OperationQueue.main
-    var timeIntervalMillis: UInt = 25
+    var timeIntervalMillis: UInt = 25 //25
     var quantizeButtons = [QuantizeButtonProtocol]()
+    var riseNum: Int = 0
+    var fallNum: Int = 0
+    var selectedSong = String()
+    var risingBeatFilter = RisingBeatFilter()
+    var fallingBeatFilter = FallingBeatFilter()
     
     //MARK: outlets
     
-    @IBOutlet weak var hamburgerButton: HamburgerIconCtrl!
-    @IBOutlet weak var settingsButton: SettingsIconCtrl!
-    @IBOutlet weak var parametersButton: ParametersButtonCtrl!
-    @IBOutlet weak var popup: PopupCtrl!
-    @IBOutlet weak var positionIndicator: PositionIndicator!
+    //@IBOutlet weak var hamburgerButton: HamburgerIconCtrl!
+    //@IBOutlet weak var settingsButton: SettingsIconCtrl!
+    //@IBOutlet weak var parametersButton: ParametersButtonCtrl!
+    //@IBOutlet weak var popup: PopupCtrl!
+    //@IBOutlet weak var positionIndicator: PositionIndicator!
+    
     @IBOutlet weak var knob: KnobCtrl!
-    @IBOutlet weak var positionLabel: UILabel!
-    @IBOutlet weak var parametersPopup: ParametersPopupCtrl!
+    
+    @IBOutlet weak var instrumentNameLabel: UILabel!
+    //@IBOutlet weak var parametersPopup: ParametersPopupCtrl!
+    
     @IBOutlet weak var playButton: PlayCtrl!
     @IBOutlet weak var recordButton: RecordCtrl!
+    
+
     @IBOutlet weak var quarterQuantizeButton: QuarterCtrl!
-    @IBOutlet weak var eighthQuantizeButton: EighthCtrl!
+    @IBOutlet weak var eightQuantizeButton: EighthCtrl!
     @IBOutlet weak var sixteenthQuantizeButton: SixteenthCtrl!
     @IBOutlet weak var thirtysecondQuantizeButton: ThirtysecondCtrl!
     @IBOutlet weak var tripletQuantizeButton: TripletCtrl!
     
     //MARK: parameters popup elements
-    @IBOutlet weak var muteButton: MuteButton!
-    @IBOutlet weak var clearButton: ClearButton!
-    @IBOutlet weak var soloButton: SoloButton!
-    @IBOutlet weak var measureCountLabel: UILabel!
-    @IBOutlet weak var measuresLabel: UILabel!
-    @IBOutlet weak var rightMeasureButton: MeasureRightButton!
-    @IBOutlet weak var leftMeasureButton: MeasureLeftButton!
+    //@IBOutlet weak var muteButton: MuteButton!
+    //@IBOutlet weak var clearButton: ClearButton!
+    //@IBOutlet weak var soloButton: SoloButton!
+    //@IBOutlet weak var measureCountLabel: UILabel!
+    //@IBOutlet weak var measuresLabel: UILabel!
+    //@IBOutlet weak var rightMeasureButton: MeasureRightButton!
+    //@IBOutlet weak var leftMeasureButton: MeasureLeftButton!
     
     //MARK: settings popup elements
     
-    @IBOutlet weak var tempoRightButton: TempoRightButton!
-    @IBOutlet weak var tempoLeftButton: TempoLeftButton!
-    @IBOutlet weak var timesigRightButton: TimeSigRightButton!
-    @IBOutlet weak var timesigLeftButton: TimeSigLeftButton!
-    @IBOutlet weak var tempoLabel: UILabel!
-    @IBOutlet weak var timesigLabel: UILabel!
+    //@IBOutlet weak var tempoRightButton: TempoRightButton!
+    //@IBOutlet weak var tempoLeftButton: TempoLeftButton!
+    //@IBOutlet weak var timesigRightButton: TimeSigRightButton!
+    //@IBOutlet weak var timesigLeftButton: TimeSigLeftButton!
+    //@IBOutlet weak var tempoLabel: UILabel!
+    //@IBOutlet weak var timesigLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         //Init settings popup with buttons and labels from main storyboard
-        popup.initButtonsAndLabels(tempoRightButtonRef: tempoRightButton, tempoLeftButtonRef: tempoLeftButton, timesigRightButtonRef: timesigRightButton, timesigLeftButtonRef: timesigLeftButton, tempoLabelRef: tempoLabel, timesigLabelRef: timesigLabel)
+        //popup.initButtonsAndLabels(tempoRightButtonRef: tempoRightButton, tempoLeftButtonRef: tempoLeftButton, timesigRightButtonRef: timesigRightButton, timesigLeftButtonRef: timesigLeftButton, tempoLabelRef: tempoLabel, timesigLabelRef: timesigLabel)
         //Init parameters popup with buttons and labels from main storyboard
-        parametersPopup.initButtonsAndLabels(clearButtonRef: clearButton, soloButtonRef: soloButton, muteButtonRef: muteButton, rightButtonRef: rightMeasureButton, leftButtonRef: leftMeasureButton, measureTitleRef: measuresLabel, measureLabelRef: measureCountLabel)
+        //parametersPopup.initButtonsAndLabels(clearButtonRef: clearButton, soloButtonRef: soloButton, muteButtonRef: muteButton, rightButtonRef: rightMeasureButton, leftButtonRef: leftMeasureButton, measureTitleRef: measuresLabel, measureLabelRef: measureCountLabel)
         
         //Connect view controller functions to buttons events
+        
+        //Play / Record
         playButton.addTarget(self, action: #selector(ViewController.playRecordButtonSelected(_:)), for: .valueChanged)
         recordButton.addTarget(self, action: #selector(ViewController.playRecordButtonSelected(_:)), for: .valueChanged)
-        hamburgerButton.addTarget(self, action: #selector(ViewController.hamburgerButtonSelected), for: .valueChanged)
+        
+        //Knob
         knob.addTarget(self, action: #selector(ViewController.knobAngleChanged(_:)), for: .valueChanged)
         knob.addTarget(self, action: #selector(ViewController.innerKnobTapped(_:)), for: .editingChanged)
         knob.addTarget(self, action: #selector(ViewController.hidePositionIndicator), for: .editingDidEnd)
-        parametersButton.addTarget(self, action: #selector(ViewController.parametersButtonSelected), for: .valueChanged)
-        parametersPopup.addTarget(self, action: #selector(ViewController.parametersOptionSelected), for: .valueChanged)
-        popup.addTarget(self, action: #selector(ViewController.popupButtonSelected), for: .valueChanged)
+        
+        //Quantize
         quarterQuantizeButton.addTarget(self, action: #selector(ViewController.quantizeButtonSelected(_:)), for: .valueChanged)
-        eighthQuantizeButton.addTarget(self, action: #selector(ViewController.quantizeButtonSelected(_:)), for: .valueChanged)
+        eightQuantizeButton.addTarget(self, action: #selector(ViewController.quantizeButtonSelected(_:)), for: .valueChanged)
         sixteenthQuantizeButton.addTarget(self, action: #selector(ViewController.quantizeButtonSelected(_:)), for: .valueChanged)
         thirtysecondQuantizeButton.addTarget(self, action: #selector(ViewController.quantizeButtonSelected(_:)), for: .valueChanged)
         tripletQuantizeButton.addTarget(self, action: #selector(ViewController.quantizeButtonSelected(_:)), for: .valueChanged)
+        quantizeButtons.append(quarterQuantizeButton)
+        quantizeButtons.append(eightQuantizeButton)
+        quantizeButtons.append(sixteenthQuantizeButton)
+        quantizeButtons.append(thirtysecondQuantizeButton)
+        quantizeButtons.append(tripletQuantizeButton)
+        
+        /*
+        hamburgerButton.addTarget(self, action: #selector(ViewController.hamburgerButtonSelected), for: .valueChanged)
+        parametersButton.addTarget(self, action: #selector(ViewController.parametersButtonSelected), for: .valueChanged)
+        parametersPopup.addTarget(self, action: #selector(ViewController.parametersOptionSelected), for: .valueChanged)
+        popup.addTarget(self, action: #selector(ViewController.popupButtonSelected), for: .valueChanged)
         
         quantizeButtons.append(quarterQuantizeButton)
         quantizeButtons.append(eighthQuantizeButton)
         quantizeButtons.append(sixteenthQuantizeButton)
         quantizeButtons.append(thirtysecondQuantizeButton)
         quantizeButtons.append(tripletQuantizeButton)
+        */
         song = Song()
-        positionLabel.text = song.selectedInstrumentName
+        //instrumentNameLabel.text = song.selectedInstrumentName
         song.start()
         
         motionManager.startAccelerometerUpdates()
         if motionManager.isAccelerometerAvailable {
             motionManager.accelerometerUpdateInterval = TimeInterval(Double(timeIntervalMillis)/1000.0)
-            motionManager.startAccelerometerUpdates(to: queue, withHandler: beatHandler as! CMAccelerometerHandler)
+            motionManager.startAccelerometerUpdates(to: queue, withHandler: beatHandler)
             
         }
     }
@@ -133,8 +138,10 @@ class ViewController: UIViewController {
     
     //MARK: hamburger / settings button
     func hamburgerButtonSelected(){
+        /*
         print("hamburger button selected")
         popup.toggleHide()
+        */
     }
     
     //MARK: Quantize buttons
@@ -162,6 +169,7 @@ class ViewController: UIViewController {
             }
         }
         
+        
         print("Triplet Quantize is \(tripletQuantizeButton.isSelected)")
         if(tripletQuantizeButton.isSelected){
             resolution = resolution * tripletQuantizeButton.resolution //ex: if quarter not selected and triplet then resolution = 1*3
@@ -170,10 +178,12 @@ class ViewController: UIViewController {
         print("updated quantize sel \(selected) res \(resolution)")
         song.updatePresetQuantize(enabled: selected, resolution: resolution)
         
+        
     }
     
     //MARK: Parameter option selected
     func parametersOptionSelected(){
+        /*
         var buttonType = ParametersButtonTypes.CLEAR
         buttonType = parametersPopup.buttonTypeSelected
         
@@ -205,10 +215,12 @@ class ViewController: UIViewController {
         case .MEASURE_RIGHT:
             print("measure change increase to \(parametersPopup.measures)!")
         }
+        */
     }
     
     //MARK: Parameter button function
     func parametersButtonSelected(){
+        /*
         //hide or show popup
         if(parametersPopup.isHidden){
             parametersPopup.show()
@@ -225,19 +237,22 @@ class ViewController: UIViewController {
                 song.updatePresetMeasureCount(newMeasures)
                 parametersPopup.measuresUpdated = false //clear for next use
             }
-            
-            
+ 
         }
+        */
     }
     
     //MARK: Hide position indicator
     func hidePositionIndicator(){
+        /*
         positionIndicator.hide()
         parametersButton.show()
+        */
     }
     
     //MARK: popup buttons handler
     func popupButtonSelected(){
+        /*
         var buttonType = SettingsButtonTypes.TEMPO_LEFT
         buttonType = popup.buttonTypeSelected
         
@@ -263,6 +278,7 @@ class ViewController: UIViewController {
             print("inc Pan to \(song.instruments[song.selectedInstrument].pan)")
             break
         }
+        */
     }
     
     //MARK: play button event handler
@@ -295,12 +311,15 @@ class ViewController: UIViewController {
     
     //MARK: Knob event handlers
     func knobAngleChanged(_ knobControl: KnobCtrl){
+        /*
         positionIndicator.show()
         parametersButton.hide()
         print("knob angle changed to \(knobControl.angle)!")
         print("knob detentNum \(knob.detent)")
         positionIndicator.setPosition(knobControl.detent)
         let position = positionIndicator.currentPos //ex: 0 or 1 or 2 or 3...17
+        */
+        let position = knob.detent
         let wasRecording = song.recordEnabled
         selectSound(position)
         if(!song.recordEnabled && wasRecording){
@@ -327,7 +346,7 @@ class ViewController: UIViewController {
     
     func selectSound(_ position: Int){
         song.selectInstrument(position)
-        positionLabel.text = song.selectedInstrumentName
+        instrumentNameLabel.text = song.selectedInstrumentName
     }
     
     func innerKnobTapped(_ knobControl: Knob){
@@ -340,24 +359,58 @@ class ViewController: UIViewController {
     }
 
     //MARK: Motion Sensor Functions
-    func beatHandler(_ data: CMAccelerometerData?, error: NSError?){
-        let valx = Int32(16383.0 * (data!.acceleration.x))
-        let valy = Int32(16383.0 * (data!.acceleration.y))
-        let valz = Int32(16383.0 * (data!.acceleration.z))
+    func beatHandler(_ data: CMAccelerometerData?, error: Error?){
         
+        //let valx = Int32(16383.0 * (data!.acceleration.x))
+        let valx_f: Double = (data!.acceleration.x)*100.0
+        let valx = Int16(valx_f)
+        
+        if fallingBeatFilter.isBeat(x: valx){
+            fallNum += 1
+            if(fallNum >= 2 || (riseNum == 0)){
+                print("Fall Note \(fallNum)")
+                song.addNote() //make drum sound and add to track if recording!
+                riseNum = 0
+                
+            }
+            
+        }
+            
+        else if risingBeatFilter.isBeat(x: valx) {
+            riseNum += 1
+            if(riseNum >= 2 || (fallNum == 0)){
+                print("Rise Note \(riseNum)")
+                song.addNote() //make drum sound and add to track if recording!
+                fallNum = 0
+                
+            }
+        }
+        
+        //let valy_f: Double = (data!.acceleration.y)*100.0
+        //let valy = Int16(valy_f)
+        
+        //let valz_f: Double = (data!.acceleration.z)*100.0
+        //let valz = Int16(valz_f)
+        
+        
+        /*
         sensor?.updateState(with: valx, andY: valy, andZ: valz, andMillisElapsed: timeIntervalMillis);
+        sensor?.printXList();
         sensor?.handleMidiEvents();
         if(sensor?.beat)!{
             let eventStatus = Int((sensor?.getEventStatus())!)
             if eventStatus != 0x80{
+                testNum += 1
+                print("Note \(testNum)")
                 song.addNote() //make drum sound and add to track if recording!
 //                updateButtonStatesAfterNoteAdded()
             }
             
         }
+        */
         
     }
     
     
-}*/
+}
 

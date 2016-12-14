e1d7bfcecf5646912e3567d11772abab046b88b7
Alex-L-Crane <alexcranedesigner@gmail.com>
2016-12-14 00:23:54 -0800

    Updated custom sliders. Added sliders to pan and volume positions in paramters popup

---
+++ proof_of_concept/apps/iOS/Groover-UI/Groover/PanSlider.swift
@@ -0,0 +1,16 @@
+//
+//  PanSlider.swift
+//  Groover
+//
+//  Created by Alex Crane on 12/13/16.
+//  Copyright © 2016 TCM. All rights reserved.
+//
+
+import UIKit
+
+class PanSlider: UIControl {
+
+    override func draw(_ rect: CGRect) {
+        UIGroover.drawPanSliderCanvas()
+    }
+}

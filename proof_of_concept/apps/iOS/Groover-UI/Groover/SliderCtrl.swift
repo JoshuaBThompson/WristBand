98415b5b9e1c07d810256975fa5e26b0b8c9c0c2
JoshuaBThompson <jbthompson.eng@gmail.com>
2016-12-17 14:55:38 -0800

    added snap to position for slider controllers

--- proof_of_concept/apps/iOS/Groover-UI/Groover/SliderCtrl.swift
+++ proof_of_concept/apps/iOS/Groover-UI/Groover/SliderCtrl.swift
@@ -17,7 +17,21 @@ class SliderCtrl: UIControl{
     var maxPosX: CGFloat = 95
     var minPosX: CGFloat = -95.0
     var position: CGPoint!
+    var snapFilter = SliderSnapFilter()
     
+    //MARK: Computed variables
+    var detent: Int {
+        return snapFilter.getDetent(self.position.x)
+    }
+    
+    var scaled_detent: CGFloat {
+        return snapFilter.getScaledDetent(self.position.x)
+    }
+    
+    var pos_from_detent: CGFloat {
+        let d = self.detent
+        return self.snapFilter.getDetentPos(d)
+    }
     
     //MARK: touch tracking functions
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
@@ -52,6 +66,8 @@ class SliderCtrl: UIControl{
     override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
         let endLocation = touch?.location(in: self)
         print("end tracking pan at \(endLocation?.x)")
+        let final_pos_x = self.pos_from_detent //snap to neartest detent
+        self.position.x = final_pos_x
         setNeedsDisplay()
         sendActions(for: .valueChanged)
     }
@@ -92,4 +108,74 @@ class SliderCtrl: UIControl{
         }
         
     }
+    
+}
+
+/*
+ SliderSnap filter class
+ Use this to create snap filter for various knob functions (instrument selection, tempo ...etc)
+ */
+
+
+class SliderSnapFilter {
+    
+    var positions = [CGFloat]() //list of positions for each detent number
+    var count: Int! //divide position range by this number to get position per detent
+    var offset: CGFloat! //position offset, where angles will start from
+    var range: CGFloat! //for ex: 0 - 100
+    var pos_per_detent: CGFloat!
+    var scale: CGFloat = 5.0
+    var scale_offset: CGFloat = 5.0
+    
+    
+    var maxPosition: CGFloat {
+        return offset
+    }
+    
+    var minPosition: CGFloat {
+        return offset - range
+    }
+    //MARK: Initialize slider snap filter
+    init(detentCount: Int = 10, posOffset: CGFloat = 0, posRange: CGFloat = 100){
+        count = detentCount
+        offset = posOffset
+        range = posRange
+        
+        //calculate pos per detents using count and range
+        //ex: if range = 360 deg and count = 10 then angle per detent = 36 deg
+        pos_per_detent = abs(range / CGFloat(count))
+        self.initPositions()
+    }
+    
+    //MARK: Initialize pos list
+    func initPositions(){
+        for i in 0..<count {
+            let pos = offset + self.pos_per_detent*CGFloat(i) //ex: 0 + 36*2 = 72
+            positions.append(pos)
+        }
+    }
+    
+    //MARK: Get detent from angle
+    func getDetent(_ pos: CGFloat)->Int{
+        let detent = abs(Int((pos-offset)/self.pos_per_detent))
+        return detent
+        
+    }
+    
+    func getScaledDetent(_ pos: CGFloat)->CGFloat{
+            let raw_detent = getDetent(pos)
+            return CGFloat(raw_detent)*self.scale + self.scale_offset
+    }
+    
+    //MARK: Get detent position
+    func getDetentPos(_ detent: Int)->CGFloat{
+        if(detent < positions.count){
+            return positions[detent]
+            
+        }
+        else{
+            return 0
+        }
+    }
+    
 }
--- proof_of_concept/apps/iOS/Groover-UI/Groover/TempoSliderCtrl.swift
+++ proof_of_concept/apps/iOS/Groover-UI/Groover/TempoSliderCtrl.swift
@@ -10,13 +10,46 @@ import Foundation
 import UIKit
 
 class TempoSliderCtrl: SliderCtrl{
-    //MARK: Computed properties
+    
+    //MARK: Properties
     let max_tempo_bpm: Int = 200
+    let min_tempo_bpm: Int = 1
+    let default_tempo_bpm: Int = 60
+    
+    //MARK: Computed properties
+    var pos_to_tempo_scale: CGFloat {
+        return CGFloat(self.max_tempo_bpm - self.min_tempo_bpm)/(self.maxPosX - self.minPosX)
+    }
+    
+    var tempo_offset: CGFloat {
+        /*
+         y = mx + b -> t = m*p + offset
+         offset = t - m*p when t = 1 p = 5
+         offset = t - m*5
+         m = max_temp - min_temp / maxPos - minPos
+         */
+        let offset = CGFloat(self.min_tempo_bpm) - (self.pos_to_tempo_scale*self.minPosX)
+        return offset
+    }
+    
+    var pos_offset: CGFloat {
+        /*
+         y = mx + b -> p = m*t + offset
+         offset = p - m*t when p = 5 t = 1
+         offset = p - m*1
+         m = max_pos - min_pos / max_tempo - min_tempo
+        */
+        let offset = self.minPosX - (CGFloat(self.min_tempo_bpm)/(self.pos_to_tempo_scale))
+        return offset
+    }
+    
     var tempo: Int {
         if(self.position==nil){
             return 1
         }
-        var tempo_bpm = Int(self.position.x * (CGFloat(self.max_tempo_bpm)/(self.maxPosX - self.minPosX)))
+        
+        var tempo_bpm = Int(self.scaled_detent)
+
         if(tempo_bpm < 1){
             tempo_bpm = 1
         }
@@ -24,30 +57,52 @@ class TempoSliderCtrl: SliderCtrl{
             tempo_bpm = self.max_tempo_bpm
         }
         
+        
         return tempo_bpm
+    }
+    
+    func init_vars(){
+        self.maxPosX = 190.0
+        self.minPosX = 5.0
+        self.snapFilter = SliderSnapFilter(detentCount: 40, posOffset: self.minPosX, posRange: self.maxPosX)
+        self.snapFilter.scale = 5.0 /* tempo is updated by 5 pbm at a time using the slider */
+        
     }
     
      override init(frame: CGRect) {
      super.init(frame: frame)
-     self.maxPosX = 190.0
-     self.minPosX = 5.0
+        init_vars()
+     
      }
      
      
      required init?(coder aDecoder: NSCoder) {
      super.init(coder: aDecoder)
-     self.maxPosX = 190.0
-     self.minPosX = 5.0
+        init_vars()
      }
     
     
     //MARK: Draw
     override func draw(_ rect: CGRect) {
         if(self.position == nil){
-            UIGroover.drawSliderCanvas()
+            self.position = CGPoint(x: 0, y: 0)
+            self.update_pos_from_tempo(new_tempo: self.default_tempo_bpm)
+            UIGroover.drawSliderCanvas(sliderPosition: self.position.x)
+            
         }
         else{
             UIGroover.drawSliderCanvas(sliderPosition: self.position.x)
+            print("detent is \(self.detent)")
+            print("scaled detent is \(self.scaled_detent)")
         }
+    }
+    
+    //MARK: Update position x from tempo
+    func update_pos_from_tempo(new_tempo: Int){
+        
+        var new_pos = CGFloat(new_tempo) * (self.maxPosX - self.minPosX) / CGFloat(self.max_tempo_bpm - self.min_tempo_bpm)
+        new_pos = new_pos + self.pos_offset
+        self.position.x = new_pos
+        print("Init temp is \(self.tempo)")
     }
 }

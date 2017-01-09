f8efc52bdfa8950d58e01511de6cb433c6a5e8e3
Alex-L-Crane <alexcranedesigner@gmail.com>
2017-01-08 17:25:34 -0800

    added edit and delete buttons to song list view

--- proof_of_concept/apps/iOS/Groover-UI/Groover/UIGroover.swift
+++ proof_of_concept/apps/iOS/Groover-UI/Groover/UIGroover.swift
@@ -1532,4 +1532,81 @@ public class UIGroover : NSObject {
         context.restoreGState()
     }
 
+    public dynamic class func drawSongDeleteCanvas(frame targetFrame: CGRect = CGRect(x: 0, y: 0, width: 75, height: 75), resizing: ResizingBehavior = .aspectFit) {
+        //// General Declarations
+        let context = UIGraphicsGetCurrentContext()!
+        
+        //// Resize to Target Frame
+        context.saveGState()
+        let resizedFrame: CGRect = resizing.apply(rect: CGRect(x: 0, y: 0, width: 75, height: 75), target: targetFrame)
+        context.translateBy(x: resizedFrame.minX, y: resizedFrame.minY)
+        context.scaleBy(x: resizedFrame.width / 75, y: resizedFrame.height / 75)
+
+
+        //// Color Declarations
+        let fillColor4 = UIColor(red: 0.620, green: 0.145, blue: 0.161, alpha: 1.000)
+        let fillColor5 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1.000)
+
+        //// deleteDot Drawing
+        let deleteDotPath = UIBezierPath()
+        deleteDotPath.move(to: CGPoint(x: 37, y: 52))
+        deleteDotPath.addCurve(to: CGPoint(x: 51, y: 38), controlPoint1: CGPoint(x: 44.73, y: 52), controlPoint2: CGPoint(x: 51, y: 45.73))
+        deleteDotPath.addCurve(to: CGPoint(x: 37, y: 24), controlPoint1: CGPoint(x: 51, y: 30.27), controlPoint2: CGPoint(x: 44.73, y: 24))
+        deleteDotPath.addCurve(to: CGPoint(x: 23, y: 38), controlPoint1: CGPoint(x: 29.27, y: 24), controlPoint2: CGPoint(x: 23, y: 30.27))
+        deleteDotPath.addCurve(to: CGPoint(x: 37, y: 52), controlPoint1: CGPoint(x: 23, y: 45.73), controlPoint2: CGPoint(x: 29.27, y: 52))
+        deleteDotPath.close()
+        deleteDotPath.usesEvenOddFillRule = true
+        fillColor4.setFill()
+        deleteDotPath.fill()
+
+
+        //// deleteLine Drawing
+        let deleteLinePath = UIBezierPath(rect: CGRect(x: 31, y: 37, width: 12, height: 2))
+        fillColor5.setFill()
+        deleteLinePath.fill()
+        
+        context.restoreGState()
+
+    }
+
+
+
+
+    @objc public enum ResizingBehavior: Int {
+        case aspectFit /// The content is proportionally resized to fit into the target rectangle.
+        case aspectFill /// The content is proportionally resized to completely fill the target rectangle.
+        case stretch /// The content is stretched to match the entire target rectangle.
+        case center /// The content is centered in the target rectangle, but it is NOT resized.
+
+        public func apply(rect: CGRect, target: CGRect) -> CGRect {
+            if rect == target || target == CGRect.zero {
+                return rect
+            }
+
+            var scales = CGSize.zero
+            scales.width = abs(target.width / rect.width)
+            scales.height = abs(target.height / rect.height)
+
+            switch self {
+                case .aspectFit:
+                    scales.width = min(scales.width, scales.height)
+                    scales.height = scales.width
+                case .aspectFill:
+                    scales.width = max(scales.width, scales.height)
+                    scales.height = scales.width
+                case .stretch:
+                    break
+                case .center:
+                    scales.width = 1
+                    scales.height = 1
+            }
+
+            var result = rect.standardized
+            result.size.width *= scales.width
+            result.size.height *= scales.height
+            result.origin.x = target.minX + (target.width - result.width) / 2
+            result.origin.y = target.minY + (target.height - result.height) / 2
+            return result
+        }
+    }
 }
